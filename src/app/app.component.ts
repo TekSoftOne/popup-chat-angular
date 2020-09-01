3bf1e7a48519d3ffc991c3d0be846e15454591aa
Tien Song Nguyen <tiennsloit@gmail.com>
2020-09-01 22:35:54 +0700

    Start chat popup angular proj

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tekchat',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'popup-chat-angular';
+}
