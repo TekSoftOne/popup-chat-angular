debd4fa52d00165155afdf8e7dcdaadfbfc1a8a4
Tien Song Nguyen <tiennsloit@gmail.com>
2020-09-01 23:54:24 +0700

    setup prod env to bundle to l.js file for all

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,19 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// Get HTML head element
+const head = document.getElementsByTagName('HEAD')[0];
+
+// Create new link Element
+const link = document.createElement('link');
+
+// set the attributes for link element
+link.rel = 'stylesheet';
+
+link.type = 'text/css';
+
+link.href = 'http://chat-api.teksoft1.com/chatjs/styles.css';
+
+// Append link element to HTML head
+head.appendChild(link);
